0c31fcb00a25f77ab547a4ad3d16554a81478463
Ehtesham1234 <ehteshamusman12344321@gmail.com>
2023-08-17 20:02:04 +0530

    modal responsive

--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -1,87 +1,90 @@
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import '../SingleContent/SingleContent.css';
-import axios from 'axios';
-import {React ,useState,useEffect} from "react";
-import { img_500, unavailable, unavailableLandscape } from '../../Config/Config';
-import YouTubeIcon from '@mui/icons-material/YouTube';
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import "../SingleContent/SingleContent.css";
+import axios from "axios";
+import { React, useState, useEffect } from "react";
+import {
+  img_500,
+  unavailable,
+  unavailableLandscape,
+} from "../../Config/Config";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 import Carousel from "../Carousel/Carousel";
-import './ContentModal.css';
-
+import "./ContentModal.css";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform : 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: "38%",
-  height:"90%",
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  borderRadius:10,
+  height: "90%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  borderRadius: 10,
   boxShadow: 24,
   p: 4,
   // backgroundColor: "white",
 };
 
-  export default function TransitionsModal({ children, media_type, id }) {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    const [content, setContent] = useState();
-    const [video, setVideo] = useState();
-  
-    const fetchData = async () => {
-      const { data } = await axios.get(
-        `https://api.themoviedb.org/3/${media_type}/${id}?api_key=664e759c938128b6f44f60ad652abcd9&language=en-US`
-      );
-  
-      setContent(data);
-      // console.log(data);
-    };
-  
-    const fetchVideo = async () => {
-      const { data } = await axios.get(
-        `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=664e759c938128b6f44f60ad652abcd9&language=en-US`
-      );
-  
-      setVideo(data.results[0]?.key);
-    };
-  
-    useEffect(() => {
-      fetchData();
-      fetchVideo();
-      // eslint-disable-next-line
-    }, []);
-  
-    return (
-      <>
-        <div
-          className="media"
-          style={{ cursor: "pointer" }}
-          color="inherit"
-          onClick={handleOpen}
-        >
-          {children}
-        </div>
-        <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          open={open}
-          onClose={handleClose}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <Fade in={open}>
+export default function TransitionsModal({ children, media_type, id }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [content, setContent] = useState();
+  const [video, setVideo] = useState();
+
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=664e759c938128b6f44f60ad652abcd9&language=en-US`
+    );
+
+    setContent(data);
+    // console.log(data);
+  };
+
+  const fetchVideo = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=664e759c938128b6f44f60ad652abcd9&language=en-US`
+    );
+
+    setVideo(data.results[0]?.key);
+  };
+
+  useEffect(() => {
+    fetchData();
+    fetchVideo();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <div
+        className="media"
+        style={{ cursor: "pointer" }}
+        color="inherit"
+        onClick={handleOpen}
+      >
+        {children}
+      </div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
           <Box sx={style}>
             {content && (
-              <div className='paper'>
+              <div className="paper">
                 <div className="ContentModal">
                   <img
                     src={
@@ -114,15 +117,15 @@ const style = {
                     {content.tagline && (
                       <i className="tagline">{content.tagline}</i>
                     )}
-  
+
                     <span className="ContentModal__description">
                       {content.overview}
                     </span>
-  
+
                     <div>
                       <Carousel id={id} media_type={media_type} />
                     </div>
-  
+
                     <Button
                       variant="contained"
                       startIcon={<YouTubeIcon />}
@@ -136,9 +139,9 @@ const style = {
                 </div>
               </div>
             )}
-            </Box>
-          </Fade>
-        </Modal>
-      </>
-    );
-  }
+          </Box>
+        </Fade>
+      </Modal>
+    </>
+  );
+}
